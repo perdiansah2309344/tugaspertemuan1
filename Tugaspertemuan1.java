4afd63a6f49572ff124a015c5c72e3569d42a026
Perdiansah <perdiansah180@gmail.com>
2025-02-10 19:31:50 +0700

    Add files via upload

---
+++ Tugaspertemuan1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugaspertemuan1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Tugaspertemuan1 {
+    public static void main(String[] args) {
+        System.out.println("Hallo World! Nama saya Perdiansah dengan NIM 2309344, Saya Senang Belajar Java.");
+    }
+}
